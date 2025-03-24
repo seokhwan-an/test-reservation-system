09b44ca078eae50baf23627b9c864788c512cf66
seokhwan-an <asok369258@naver.com>
2025-03-24 23:24:33 +0900

    feat : postgresql 연동

--- test_schedule_reservation_system/test_schedule_reservation_system/settings.py
+++ test_schedule_reservation_system/test_schedule_reservation_system/settings.py
@@ -75,9 +75,13 @@ WSGI_APPLICATION = 'test_schedule_reservation_system.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'grepp',
+        'USER': 'user',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '15432',
     }
 }
 
