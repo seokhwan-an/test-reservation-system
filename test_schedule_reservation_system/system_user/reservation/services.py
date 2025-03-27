15e3ec3302089224ca3ea4b0a2f568a1c34b2f05
seokhwan-an <asok369258@naver.com>
2025-03-28 06:08:36 +0900

    feat : 유저가 자신의 예약을 수정하는 api 추가
    세부사항
    - 존재하지 않는 예약 수정 시 예외처리
    - 다른 기업의 예약 수정 시 예외처리
    - 시험 날짜 및 시간은 현재 기준 3일 이내로 변경 시 예외처리
    - 인원 뱐경 시 그 시간 대의 총 인원의 수가 50,000명을 넘을 경우 예외처리
    - 시험 종료 시간이 시험 시작 시간보다 빠르면 예외처리
    - 이미 confirm 된 예약 수정 요청 시 예외처리

--- test_schedule_reservation_system/system_user/reservation/services.py
+++ test_schedule_reservation_system/system_user/reservation/services.py
@@ -1,8 +1,10 @@
-from core.models.reservation import Reservation
+from core.models.reservation import Reservation, Status
+from core.models.reservations import Reservations
 from django.contrib.auth.models import User
 from rest_framework.exceptions import NotFound, PermissionDenied, ValidationError
+from rest_framework.utils.serializer_helpers import ReturnDict
 
-from .serializers import ReservationSerializer
+from .serializers import ReservationSerializer, ReservationUpdateSerializer
 
 
 def get_my_reservations(user: User) -> ReservationSerializer:
@@ -17,13 +19,40 @@ def get_reservation_by_id(reservation_id: int) -> Reservation:
         raise NotFound("해당 예약이 존재하지 않습니다.")
 
 
-def delete_reservation(reservation_id: int, user: User):
-    reservation = get_reservation_by_id(reservation_id)
-
+def is_manipulate(reservation: Reservation, user: User, message: str):
     if not reservation.is_writer(user):
-        raise PermissionDenied("본인의 예약만 삭제할 수 있습니다.")
+        raise PermissionDenied(f"본인의 예약만 {message}할 수 있습니다.")
 
     if reservation.is_confirmed():
-        raise ValidationError("확정된 예약은 삭제할 수 없습니다.")
+        raise ValidationError(f"확정된 예약은 {message}할 수 없습니다.")
+
+
+def update_reservation(reservation_id: int, data: ReturnDict, user: User):
+    reservation = get_reservation_by_id(reservation_id)
+    is_manipulate(reservation, user, '수정')
+
+    serializer = ReservationUpdateSerializer(data=data, partial=True)
+    serializer.is_valid(raise_exception=True)
+
+    new_headcount = serializer.validated_data.get('headcount', reservation.headcount)
+
+    reservations_in_time = Reservations(Reservation.objects.filter(
+        test_reservation_date=reservation.test_reservation_date,
+        test_start_time__lt=reservation.test_end_time,
+        test_end_time__gt=reservation.test_start_time,
+        status=Status.CONFIRM
+    ).exclude(pk=reservation.pk))
+
+    reservations_in_time.validate_exceed_limit(new_headcount)
+
+    for attr, value in serializer.validated_data.items():
+        setattr(reservation, attr, value)
+
+    reservation.save()
+
+
+def delete_reservation(reservation_id: int, user: User):
+    reservation = get_reservation_by_id(reservation_id)
+    is_manipulate(reservation, user, '삭제')
 
     reservation.delete()
