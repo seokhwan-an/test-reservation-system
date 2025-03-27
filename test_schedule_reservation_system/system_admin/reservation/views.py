520aba70320351db106b50217b0f4b0f5eefed5d
seokhwan-an <asok369258@naver.com>
2025-03-28 00:53:17 +0900

    feat : 어드민에서 전체 예약을 삭제하는 기능 추가

--- test_schedule_reservation_system/system_admin/reservation/views.py
+++ test_schedule_reservation_system/system_admin/reservation/views.py
@@ -1,3 +1,4 @@
+from rest_framework.exceptions import NotFound
 from rest_framework.viewsets import ViewSet
 from rest_framework.decorators import action
 from rest_framework.response import Response
@@ -15,3 +16,13 @@ class ReservationAdminViewSet(ViewSet):
         reservations = Reservation.objects.select_related('user').all()
         serializer = ReservationSerializer(reservations, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+    @action(detail=True, methods=['delete'], url_path='delete')
+    def delete_reservation(self, request, pk=None):
+        try:
+            reservation = Reservation.objects.get(pk=pk)
+        except Reservation.DoesNotExist:
+            raise NotFound("해당 예약이 존재하지 않습니다.")
+
+        reservation.delete()
+        return Response({'message': '예약이 삭제되었습니다.'}, status=status.HTTP_204_NO_CONTENT)
--- test_schedule_reservation_system/system_admin/tests/test_reservation.py
+++ test_schedule_reservation_system/system_admin/tests/test_reservation.py
@@ -58,3 +58,56 @@ class TestReservationAdminList(APITestCase):
         response = self.client.get(self.url)
 
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+
+class TestDeleteReservation(APITestCase):
+    def setUp(self):
+        self.admin = User.objects.create_user(
+            username='admin',
+            password='adminpass',
+            is_staff=True
+        )
+
+        self.normal_user = User.objects.create_user(
+            username='user',
+            password='userpass',
+            is_staff=False
+        )
+
+        self.reservation = Reservation.objects.create(
+            user=self.admin,
+            test_reservation_date=date.today(),
+            test_start_time=time(10, 0),
+            test_end_time=time(11, 0),
+            headcount=2
+        )
+
+        self.url = f'/api/admin-reservation/{self.reservation.id}/delete/'
+
+    def test_admin_can_delete_reservation(self):
+        tokens = generate_tokens_for_user(self.admin)
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + tokens['access_token'])
+
+        response = self.client.delete(self.url)
+
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertFalse(Reservation.objects.filter(id=self.reservation.id).exists())
+
+    def test_normal_user_cannot_delete_reservation(self):
+        tokens = generate_tokens_for_user(self.normal_user)
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + tokens['access_token'])
+
+        response = self.client.delete(self.url)
+
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertTrue(Reservation.objects.filter(id=self.reservation.id).exists())
+
+    def test_delete_nonexistent_reservation(self):
+        tokens = generate_tokens_for_user(self.admin)
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + tokens['access_token'])
+
+        invalid_url = '/api/admin/reservations/reservations/9999/delete/'
+
+        response = self.client.delete(invalid_url)
+
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
